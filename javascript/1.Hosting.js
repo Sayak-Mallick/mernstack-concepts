f838f7e99fa591348778225d7eaf68afdbb92087
Sayak-Mallick <sayakmallickkv@gmail.com>
2026-03-21 12:23:46 +0530

    Hositing done

---
+++ javascript/1.Hosting.js
@@ -0,0 +1,20 @@
+console.log(x); // undefined
+var x = 5;
+console.log(x); // 5
+
+sayHello();
+
+function sayHello() {
+  console.log("Hello");
+}
+
+// console.log(y);
+// let y = 10;
+// console.log(y);
+
+sayHi();
+
+let sayHi = function () {
+  console.log("Hi");
+};
+
