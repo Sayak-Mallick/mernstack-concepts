a74c6923bf911b80f60e7f4e6a924c9db1cedd6b
Sayak Mallick <63100600+Sayak-Mallick@users.noreply.github.com>
2026-03-25 13:49:13 +0000

    Promise Updated

--- javascript/6.Promises.js
+++ javascript/6.Promises.js
@@ -10,3 +10,16 @@ const p = new Promise((resolve, reject) => {
 p.then((data) => console.log(data))
   .catch((err) => console.log(err))
   .finally(() => console.log("Done"));
+
+const MyPromise = new Promise((resolve, reject) => {
+  const sum = 5 + 5;
+  if (sum === 10) {
+    resolve("Fulfilled");
+  } else {
+    reject("Failed");
+  }
+});
+
+MyPromise.then((data) => console.log(data))
+  .catch((err) => console.error(err))
+  .finally(() => console.log("Done ✅"));
